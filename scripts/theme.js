a4ab96153eceb86474ed3a25e3575009e98cf828
Mason Lawlor <mason.lawlor@me.com>
2014-06-16 19:45:12 -0500

    the creation

---
+++ scripts/theme.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+"use strict";
+
+  // FlexSlider
+  $('.slider').flexslider({
+    animation: 'slide',
+    selector: '.slides > li',
+    controlNav: false,
+    directionNav: true,
+    slideshowSpeed: 3600,
+    animationSpeed: 1200,
+    prevText: '',
+    nextText: ''
+  });
+
+  // Clock
+  $('.clock').countdown('2014/10/10').on('update.countdown', function(event) {
+    var $this = $(this).html(event.strftime(''
+    + '<div>%D<br><span>day%!d</span></div>'
+    + '<div>%H<br><span>hour%!H</span></div>'
+    + '<div>%M<br><span>minute%!M</span></div>'
+    + '<div>%S<br><span>second%!S</span></div>'));
+  });
+
+  // Parallax effect
+  $('.header').parallax("50%", 0.2);
+
+});
